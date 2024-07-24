d14a750f9e694e7430c21b0fc82cf055f1058509
LXServer <35785095+Angel892@users.noreply.github.com>
2024-07-24 17:48:18 -0600

    feat: separando los archivos

--- functions/actualizarScript.sh
+++ functions/actualizarScript.sh
@@ -7,24 +7,34 @@ actualizar_script() {
     echo -e "\e[1;34m=========================\e[0m"
     cd /etc/vpsmanager
 
-    # Elimina el script antiguo (opcional)
-    sudo rm /etc/vpsmanager/adm.sh
-
     # Obtener el ID del último commit
     OWNER="Angel892"
     REPO="vpsmanager"
     BRANCH="master"
 
     # Hacer una solicitud a la API de GitHub para obtener el último commit en la rama especificada
-    LATEST_COMMIT=$(curl -s https://api.github.com/repos/$OWNER/$REPO/commits/$BRANCH | jq -r '.sha')
+    LATEST_COMMIT=$(curl -s https://api.github.com/repos/$OWNER/$REPO/commits/$BRANCH | jq -r '.[0].sha')
 
-    sudo wget -O /etc/vpsmanager/adm.sh https://raw.githubusercontent.com/$OWNER/$REPO/$LATEST_COMMIT/adm.sh
+    # Descargar el repositorio completo
+    sudo rm -rf /tmp/vpsmanager
+    sudo git clone https://github.com/$OWNER/$REPO.git /tmp/vpsmanager
     if [ $? -ne 0 ]; then
-        echo -e "\e[1;31mError: Falló la actualización del script.\e[0m"
+        echo -e "\e[1;31mError: Falló la clonación del repositorio.\e[0m"
         read -p "Presione Enter para continuar..."
         return
     fi
+
+    # Copiar los archivos al directorio de destino
+    sudo cp -r /tmp/vpsmanager/* /etc/vpsmanager/
+    if [ $? -ne 0 ]; then
+        echo -e "\e[1;31mError: Falló la copia de los archivos.\e[0m"
+        read -p "Presione Enter para continuar..."
+        return
+    fi
+
+    # Asignar permisos de ejecución al script principal y los archivos de funciones
     sudo chmod +x /etc/vpsmanager/adm.sh
+    sudo chmod +x /etc/vpsmanager/funciones/*.sh
     echo -e "\e[1;32mEl script se ha actualizado correctamente.\e[0m"
     read -p "Presione Enter para continuar..."
     exec /etc/vpsmanager/adm.sh
