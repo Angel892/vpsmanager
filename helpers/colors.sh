276c29635738096e78f2e211b1a3a5208786408f
LXServer <35785095+Angel892@users.noreply.github.com>
2024-07-25 12:27:50 -0600

    feat: cambiando colores

--- helpers/colors.sh
+++ helpers/colors.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 PRINCIPAL="\e[1;34m";
-SECUNDARIO="\e[1;32m";
+SECUNDARIO="\e[1;33m";
 INFO="\e[1;33m";
 
-ROJO="\e[1;32m";
-VERDE="\e[1;31m";
+ROJO="\e[1;31m";
+VERDE="\e[1;32m";
 
 SALIR="\e[1;31m";
 NC="\e[0m";
