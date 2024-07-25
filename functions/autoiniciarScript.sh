4727779354fafa0bedba5b6f7317b0497ae59b6c
LXServer <35785095+Angel892@users.noreply.github.com>
2024-07-25 17:21:59 -0600

    TEST: MENU PROTOCOL

--- functions/autoiniciarScript.sh
+++ functions/autoiniciarScript.sh
@@ -7,22 +7,18 @@ source $HELPERS_PATH/colors.sh
 #funciones globales
 source $HELPERS_PATH/global.sh
 
-
 autoiniciarScript() {
     clear
     echo -e "${PRINCIPAL}=========================${NC}"
     echo -e "${PRINCIPAL}  Autoiniciar Script${NC}"
     echo -e "${PRINCIPAL}=========================${NC}"
 
-    local crontab_entry="@reboot /etc/vpsmanager/adm.sh"
-
-    # Verificar si ya existe una entrada en crontab
-    if crontab -l | grep -q "$crontab_entry"; then
-        echo -e "${VERDE}El script ya está configurado para autoiniciarse al entrar en la VPS.${NC}"
+    if grep -q "/etc/vpsmanager/adm.sh" /etc/bash.bashrc; then
+        echo -e "${VERDE}El script ya está configurado para autoiniciarse.${NC}"
         echo -e "${AMARILLO}¿Desea eliminar esta configuración? (s/n):${NC}"
         read -p "Opción: " opcion
         if [[ "$opcion" =~ ^[sS]$ ]]; then
-            (crontab -l | grep -v "$crontab_entry") | crontab -
+            sed -i '/\/etc\/vpsmanager\/adm.sh/d' /etc/bash.bashrc
             echo -e "${ROJO}Autoinicio del script desactivado.${NC}"
         else
             echo -e "${AMARILLO}Operación cancelada.${NC}"
@@ -32,7 +28,8 @@ autoiniciarScript() {
         echo -e "${AMARILLO}¿Desea activar esta configuración? (s/n):${NC}"
         read -p "Opción: " opcion
         if [[ "$opcion" =~ ^[sS]$ ]]; then
-            (crontab -l 2>/dev/null; echo "$crontab_entry") | crontab -
+            cp /etc/bash.bashrc /etc/bash.bashrc.bak
+            echo '/etc/vpsmanager/adm.sh' >>/etc/bash.bashrc
             echo -e "${VERDE}Autoinicio del script activado.${NC}"
         else
             echo -e "${AMARILLO}Operación cancelada.${NC}"
