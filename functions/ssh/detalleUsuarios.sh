a122fba728914bc5e40b651591e8a254031a1fe4
LXServer <35785095+Angel892@users.noreply.github.com>
2024-07-25 14:11:08 -0600

    feat: administracion de cuentas ssj

--- functions/ssh/detalleUsuarios.sh
+++ functions/ssh/detalleUsuarios.sh
@@ -16,23 +16,26 @@ detalleUsuariosSSH() {
     echo -e "${PRINCIPAL}=========================${NC}"
 
     # Encabezados de la tabla
-    echo -e "${AMARILLO}Usuario\tFecha de Expiración\tLímite de Conexiones${NC}" > /tmp/user_details.txt
+    echo -e "${AMARILLO}Usuario\tFecha de Expiración (Días Restantes)\tLímite de Conexiones${NC}" > /tmp/user_details.txt
 
-    # Recorrer cada usuario en /etc/passwd
-    while IFS=: read -r username _ _ _ _ _ home _; do
+    # Obtener todos los usuarios y sus detalles de una sola vez
+    users=$(cut -d: -f1 /etc/passwd)
+    limits=$(grep "maxlogins" /etc/security/limits.conf)
+
+    for username in $users; do
         # Obtener la fecha de expiración
         expiration_date=$(sudo chage -l "$username" | grep "Account expires" | awk -F": " '{print $2}')
         
         # Calcular los días restantes
         if [ "$expiration_date" != "never" ]; then
             expiration_days=$(( ($(date -d "$expiration_date" +%s) - $(date +%s)) / 86400 ))
-            expiration_info="$expiration_date[$expiration_days días]"
+            expiration_info="$expiration_date[$expiration_days]"
         else
             expiration_info="Nunca"
         fi
 
         # Obtener el límite de conexiones
-        connection_limit=$(sudo grep "$username" /etc/security/limits.conf | grep "maxlogins" | awk '{print $4}')
+        connection_limit=$(echo "$limits" | grep "$username" | awk '{print $4}')
         
         if [ -z "$connection_limit" ]; then
             connection_limit="Sin límite"
@@ -40,7 +43,7 @@ detalleUsuariosSSH() {
         
         # Agregar los detalles del usuario al archivo temporal
         echo -e "${BLANCO}$username\t$expiration_info\t$connection_limit${NC}" >> /tmp/user_details.txt
-    done < /etc/passwd
+    done
 
     # Mostrar la tabla formateada
     column -t -s $'\t' /tmp/user_details.txt
