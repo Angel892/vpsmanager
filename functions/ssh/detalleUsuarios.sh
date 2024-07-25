685c4bdcbf53275c2d84270b796f17d1c0cb8702
LXServer <35785095+Angel892@users.noreply.github.com>
2024-07-25 14:02:23 -0600

    feat: administracion de cuentas ssj

--- functions/ssh/detalleUsuarios.sh
+++ functions/ssh/detalleUsuarios.sh
@@ -7,9 +7,30 @@ source $HELPERS_PATH/colors.sh
 #funciones globales
 source $HELPERS_PATH/global.sh
 
-MAIN_PATH="/etc/vpsmanager";
+MAIN_PATH="/etc/vpsmanager"
 
 detalleUsuariosSSH() {
-    cut -d: -f1 /etc/passwd
+    clear
+    echo -e "${PRINCIPAL}=========================${NC}"
+    echo -e "${PRINCIPAL}  Detalles de usuarios SSH${NC}"
+    echo -e "${PRINCIPAL}=========================${NC}"
+
+    while IFS=: read -r username _ _ _ _ _ home _; do
+        # Obtener la fecha de expiración
+        expiration_date=$(sudo chage -l "$username" | grep "Account expires" | awk -F": " '{print $2}')
+        # Obtener el límite de conexiones
+        connection_limit=$(sudo grep "$username" /etc/security/limits.conf | grep "maxlogins" | awk '{print $4}')
+
+        if [ -z "$connection_limit" ]; then
+            connection_limit="Sin límite"
+        fi
+
+        # Mostrar detalles del usuario
+        echo -e "${VERDE}Usuario: ${NC}$username"
+        echo -e "${VERDE}Fecha de expiración: ${NC}$expiration_date"
+        echo -e "${VERDE}Límite de conexiones: ${NC}$connection_limit"
+        echo -e "${PRINCIPAL}=========================${NC}"
+    done </etc/passwd
+
     read -p "Presione Enter para continuar..."
 }
