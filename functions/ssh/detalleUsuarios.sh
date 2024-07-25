00114d2b0edccc2f72e0bfdac1bc1cd39faef19b
LXServer <35785095+Angel892@users.noreply.github.com>
2024-07-25 14:08:34 -0600

    feat: administracion de cuentas ssj

--- functions/ssh/detalleUsuarios.sh
+++ functions/ssh/detalleUsuarios.sh
@@ -16,12 +16,21 @@ detalleUsuariosSSH() {
     echo -e "${PRINCIPAL}=========================${NC}"
 
     # Encabezados de la tabla
-    echo -e "${AMARILLO}Usuario\tFecha de Expiración\tLímite de Conexiones${NC}" > /tmp/user_details.txt
+    echo -e "${AMARILLO}Usuario\tFecha de Expiración (Días Restantes)\tLímite de Conexiones${NC}" > /tmp/user_details.txt
 
     # Recorrer cada usuario en /etc/passwd
     while IFS=: read -r username _ _ _ _ _ home _; do
         # Obtener la fecha de expiración
         expiration_date=$(sudo chage -l "$username" | grep "Account expires" | awk -F": " '{print $2}')
+        
+        # Calcular los días restantes
+        if [ "$expiration_date" != "never" ]; then
+            expiration_days=$(( ($(date -d "$expiration_date" +%s) - $(date +%s)) / 86400 ))
+            expiration_info="$expiration_date [$expiration_days días]"
+        else
+            expiration_info="Nunca"
+        fi
+
         # Obtener el límite de conexiones
         connection_limit=$(sudo grep "$username" /etc/security/limits.conf | grep "maxlogins" | awk '{print $4}')
         
@@ -30,7 +39,7 @@ detalleUsuariosSSH() {
         fi
         
         # Agregar los detalles del usuario al archivo temporal
-        echo -e "${BLANCO}$username\t$expiration_date\t$connection_limit${NC}" >> /tmp/user_details.txt
+        echo -e "${BLANCO}$username\t$expiration_info\t$connection_limit${NC}" >> /tmp/user_details.txt
     done < /etc/passwd
 
     # Mostrar la tabla formateada
@@ -40,4 +49,3 @@ detalleUsuariosSSH() {
     echo -e "${PRINCIPAL}=========================${NC}"
     read -p "Presione Enter para continuar..."
 }
-
