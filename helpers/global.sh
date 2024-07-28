dc632f9d4fbffa92ead048e51e57cbe2648a17dd
Angel Amaro <alex.jaj94@gmail.com>
2024-07-27 21:19:17 -0600

    feat: reparacion de menu en 'Volver al menu anterior'

--- helpers/global.sh
+++ helpers/global.sh
@@ -51,17 +51,17 @@ selectionFun() {
         # Verifica si la opción es válida
         if [[ -z $selection ]]; then
             # Si la entrada está vacía, muestra un mensaje de error y repite el bucle
-            echo -ne "${eliminarl}\033[1;31mPor favor, ingrese una opción válida.\033[0m" >&2
+            echo -ne "\033[1;31mPor favor, ingrese una opción válida.\033[0m" >&2
             sleep 1
-            echo -e "${eliminarl}" >&2
+            echo -ne "${eliminarl} ${eliminarl}" >&2
         elif [[ $options =~ (^|[^\d])$selection($|[^\d]) ]]; then
             echo $selection
             break
         else
             # Si la opción no es válida, muestra el mensaje de error y repite el bucle
-            echo -ne "${eliminarl}\033[1;31mSelección no válida: $selection\033[0m" >&2
+            echo -ne "\033[1;31mSelección no válida: $selection\033[0m" >&2
             sleep 1
-            echo -e "${eliminarl}" >&2
+            echo -ne "${eliminarl} ${eliminarl}" >&2
         fi
     done
 }
