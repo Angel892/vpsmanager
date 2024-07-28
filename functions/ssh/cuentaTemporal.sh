1f70300df3904c54313ee9262f3a580883323ce0
Angel Amaro <alex.jaj94@gmail.com>
2024-07-28 11:19:18 -0600

    feat: avance usuario temporal

--- functions/ssh/cuentaTemporal.sh
+++ functions/ssh/cuentaTemporal.sh
@@ -2,7 +2,7 @@
 
 crearCuentaTemporalSSH() {
 
-    local temporalPath="$mainPath/temp/demo-ssh";
+    local temporalPath="$mainPath/temp/demo-ssh"
 
     rm -rf $temporalPath 2>/dev/null
     mkdir $temporalPath 2>/dev/null
@@ -31,54 +31,57 @@ crearCuentaTemporalSSH() {
         exit" >/tmp/$2
     }
 
-    clear && clear
-    msg -bar
-    msg -tit
-    msg -bar
-    msgCentrado -amarillo "CREAR USUARIO POR TIEMPO (Minutos)"
-    msg -bar
-    msg -blanco "Los Usuarios que cres en esta opcion se eliminaran\n automaticamete pasando el tiempo designado."
-    msg -bar
-    msg -blanco "Digite Nuevo Usuario: " && read name
-    if [[ -z $name ]]; then
-        msg -rojo "No a digitado el Nuevo Usuario"
-        exit
-    fi
-    if cat /etc/passwd | grep $name: | grep -vi [a-z]$name | grep -v [0-9]$name >/dev/null; then
-        msg -rojo "Usuario $name ya existe"
-        exit
-    fi
-    msg -blanco "Digite Nueva Contraseña: " && read pass
-    msg -blanco "Digite Tiempo (Minutos): " && read tmp
-    if [ "$tmp" = "" ]; then
-        tmp="30"
-        msg -verde "Fue Definido 30 minutos Por Defecto!"
+    while true; do
+        clear && clear
         msg -bar
-        sleep 2s
-    fi
-    useradd -m -s /bin/false $name
-    (
-        echo $pass
-        echo $pass
-    ) | passwd $name 2>/dev/null
-    touch /tmp/$name
-    tmpusr $tmp $name
-    chmod 777 /tmp/$name
-    touch /tmp/cmd
-    chmod 777 /tmp/cmd
-    echo "nohup /tmp/$name & >/dev/null" >/tmp/cmd
-    /tmp/cmd 2>/dev/null 1>/dev/null
-    rm -rf /tmp/cmd
-    touch $temporalPath/$name
-    echo "senha: $pass" >>$temporalPath/$name
-    echo "data: ($tmp)Minutos" >>$temporalPath/$name
-    msg -bar
-    msgCentrado -verde "¡¡  USUARIO TEMPORAL x MINUTOS  !!"
-    msg -bar
-    echo -e "\033[1;97m\e[38;5;202m IP del Servidor: \033[1;32m$(vpsIP) "
-    echo -e "\e[38;5;202m Usuario: \033[1;32m$name"
-    echo -e "\e[38;5;202m Contraseña: \033[1;32m$pass"
-    echo -e "\e[38;5;202m Minutos de Duración: \033[1;32m$tmp"
-    msg -bar
-    msgCentradoRead -blanco "<< Presiona enter para Continuar >>"
+        msg -tit
+        msg -bar
+        msgCentrado -amarillo "CREAR USUARIO POR TIEMPO (Minutos)"
+        msg -bar
+        msg -blanco "Los Usuarios que cres en esta opcion se eliminaran\n automaticamete pasando el tiempo designado."
+        msg -bar
+        msg -blanco "Digite Nuevo Usuario: " && read name
+        if [[ -z $name ]]; then
+            msg -rojo "No a digitado el Nuevo Usuario"
+            continue
+        fi
+        if cat /etc/passwd | grep $name: | grep -vi [a-z]$name | grep -v [0-9]$name >/dev/null; then
+            msg -rojo "Usuario $name ya existe"
+            continue
+        fi
+        msg -blanco "Digite Nueva Contraseña: " && read pass
+        msg -blanco "Digite Tiempo (Minutos): " && read tmp
+        if [ "$tmp" = "" ]; then
+            tmp="30"
+            msg -verde "Fue Definido 30 minutos Por Defecto!"
+            msg -bar
+            sleep 2s
+        fi
+        useradd -m -s /bin/false $name
+        (
+            echo $pass
+            echo $pass
+        ) | passwd $name 2>/dev/null
+        touch /tmp/$name
+        tmpusr $tmp $name
+        chmod 777 /tmp/$name
+        touch /tmp/cmd
+        chmod 777 /tmp/cmd
+        echo "nohup /tmp/$name & >/dev/null" >/tmp/cmd
+        /tmp/cmd 2>/dev/null 1>/dev/null
+        rm -rf /tmp/cmd
+        touch $temporalPath/$name
+        echo "senha: $pass" >>$temporalPath/$name
+        echo "data: ($tmp)Minutos" >>$temporalPath/$name
+        msg -bar
+        msgCentrado -verde "¡¡  USUARIO TEMPORAL x MINUTOS  !!"
+        msg -bar
+        echo -e "\033[1;97m\e[38;5;202m IP del Servidor: \033[1;32m$(vpsIP) "
+        echo -e "\e[38;5;202m Usuario: \033[1;32m$name"
+        echo -e "\e[38;5;202m Contraseña: \033[1;32m$pass"
+        echo -e "\e[38;5;202m Minutos de Duración: \033[1;32m$tmp"
+        msg -bar
+        msgCentradoRead -blanco "<< Presiona enter para Continuar >>"
+        break;
+    done
 }
