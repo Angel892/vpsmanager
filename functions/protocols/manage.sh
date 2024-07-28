3e58c421db9d865ce3f81e6f204d2658853a0c16
Angel Amaro <alex.jaj94@gmail.com>
2024-07-28 00:05:33 -0600

    feat: reparacion de menu en 'Volver al menu anterior'45

--- functions/protocols/manage.sh
+++ functions/protocols/manage.sh
@@ -44,7 +44,7 @@ menuProtocols() {
         option[$num]="nodejs"
         let num++
 
-        msgCentradoBarra -blanco "PROTOCOLOS NET"
+        msgCentradoBarra -verde "PROTOCOLOS NET"
 
         # BADVPN
         opcionMenu -blanco $num "BADVPN" true 0 "badvpn"
@@ -111,7 +111,7 @@ menuProtocols() {
         option[$num]="psiphone"
         let num++
 
-        msgCentradoBarra -blanco "PROXY´S"
+        msgCentradoBarra -verde "PROXY´S"
 
         # WEB SOCKET
         opcionMenu -blanco $num "WEBSOKET STATUS EDITABLE" true 0 "pydic-*"
