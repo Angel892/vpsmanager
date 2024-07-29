9ac89807ea8e4abbc3245f70dcdf5472767754e4
LXServer <35785095+Angel892@users.noreply.github.com>
2024-07-29 14:25:59 -0600

    feat: limitador

--- functions/ssh/manage.sh
+++ functions/ssh/manage.sh
@@ -199,7 +199,7 @@ menuSSH() {
         let num++
 
         # LIMITADOR DE CUENTAS
-        VERY="$(ps aux | grep "$(verif_fun)" | grep -v grep)"
+        VERY="$(ps aux | grep $(verif_fun) | grep -v grep)"
         [[ -z ${VERY} ]] && verificar="\e[1;93m[\033[1;31m DESACTIVADO \e[1;93m]" || verificar="\e[1;93m[\033[1;32m ACTIVO \e[1;93m]"
         opcionMenu -blanco $num "Limitador de cuentas ${VERY}"
         option[$num]="limitador"
