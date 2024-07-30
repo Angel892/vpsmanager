6a615620904a5451b590923363f8c8e854706ddc
LXServer <35785095+Angel892@users.noreply.github.com>
2024-07-30 12:30:45 -0600

    feat: python

--- functions/protocols/python.sh
+++ functions/protocols/python.sh
@@ -1,5 +1,33 @@
 menuPython() {
 
+    instalar() {
+        clear && clear
+        msg -bar
+        msgCentrado -blanco "INSTALACION DE PYTHON"
+        msg -bar
+
+        msgCentrado -amarillo "ACTUALIZANDO PAQUETES"
+        fun_bar "sudo apt update"
+
+        msgCentrado -amarillo "INSTALANDO PYTHON"
+        fun_bar "sudo apt install python3 -y"
+
+    }
+
+    detener() {
+        clear && clear
+        msg -bar
+        msgCentrado -amarillo "DESINSTALACION PYTHON"
+        msg -bar
+
+        msgCentrado -amarillo "DESINSTALANDO PYTHON"
+        fun_bar "sudo apt-get remove --purge python3 -y"
+
+        msg -bar
+        msgCentrado -verde "PYTHON DESINSTALADO CON ÉXITO"
+        msg -bar
+    }
+
     local num=1
 
     clear
@@ -26,13 +54,13 @@ menuPython() {
     msg -bar
     selection=$(selectionFun $num)
     case ${option[$selection]} in
-    "instalar") crearCuentaSSH ;;
-    "detener") crearCuentaTemporalSSH ;;
+    "instalar") instalar ;;
+    "detener") detener ;;
     "volver") return ;;
-    *) 
-        echo -e "${SALIR}Opción inválida, por favor intente de nuevo.${NC}" 
+    *)
+        echo -e "${SALIR}Opción inválida, por favor intente de nuevo.${NC}"
         sleep 2
         menuPython
-    ;;
+        ;;
     esac
 }
