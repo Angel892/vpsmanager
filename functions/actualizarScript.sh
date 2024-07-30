e18c3ff7f21d8094f1491d187dc53620e48ceafe
LXServer <35785095+Angel892@users.noreply.github.com>
2024-07-30 16:45:38 -0600

    feat: actualizando instalacion dele script

--- functions/actualizarScript.sh
+++ functions/actualizarScript.sh
@@ -2,14 +2,12 @@
 
 HELPERS_PATH="/etc/vpsmanager/helpers"
 
-
-
 actualizar_script() {
     clear
     msg -bar
     msgCentrado -amarillo "Actualizando el Script"
     msg -bar
-    
+
     cd /etc/vpsmanager
 
     # Restablecer cualquier cambio local y obtener las actualizaciones más recientes
@@ -31,14 +29,18 @@ actualizar_script() {
 
     # Asignar permisos de ejecución al script principal y a los scripts de funciones
     msgInstall "Habilitando permisos"
-    barra_intallb "sudo find /etc/vpsmanager/ -type f -name "*.sh" -exec chmod +x {} \;"
+    sudo find /etc/vpsmanager/ -type f -name "*.sh" -exec chmod +x {} \;
     if [ $? -ne 0 ]; then
         msgCentrado -rojo "Error: No se pudieron asignar los permisos de ejecución."
         msgCentradoRead -blanco "<< Presiona enter para Continuar >>"
         return
     fi
 
+    msg -bar
+
     msgCentrado -verde "El script se ha actualizado correctamente."
+
+    msg -bar
     msgCentradoRead -blanco "<< Presiona enter para Continuar >>"
     exec /etc/vpsmanager/adm.sh
 }
