aae6f5db098e473e5f883b94d4be9fd7acdb2a73
LXServer <35785095+Angel892@users.noreply.github.com>
2024-07-31 14:01:20 -0600

    feat: ssh

--- functions/protocols/dropbear.sh
+++ functions/protocols/dropbear.sh
@@ -1,36 +1,39 @@
 proto_dropbear() {
     activar_dropbear() {
 
-        clear && clear
+        showCabezera "INSTALADOR DROPBEAR | SCRIPT LXServer"
+        msgCentrado -blanco "Puede activar varios puertos en orden secuencial"
+        msgCentrado -verde "Ejemplo: 442 443 444"
         msg -bar
-        msg -tit
-        msg -bar
-        msgCentrado -amarillo "INSTALADOR DROPBEAR | SCRIPT LXServer"
-        msg -bar
-        echo -e "\033[1;97m Puede activar varios puertos en orden secuencial\n Ejemplo: \033[1;32m 442 443 444\033[1;37m"
-        msg -bar
-        echo -ne "\033[1;97m Digite  Puertos:\033[1;32m" && read -p " " -e -i "444 445" DPORT
+
+        msgne -blanco "Digite  Puertos: "
+        read -p " " -e -i "444 445" DPORT
+
         tput cuu1 && tput dl1
         TTOTAL2=($DPORT)
         for ((i = 0; i < ${#TTOTAL2[@]}; i++)); do
             [[ $(mportas | grep "${TTOTAL2[$i]}") = "" ]] && {
-                echo -e "\033[1;33m Puerto Elegido:\033[1;32m ${TTOTAL2[$i]} OK"
+                msgne -blanco "Puerto Elegido: "
+                msg -verde "${TTOTAL2[$i]} OK"
                 PORT2="$PORT2 ${TTOTAL2[$i]}"
             } || {
-                echo -e "\033[1;33m Puerto Elegido:\033[1;31m ${TTOTAL2[$i]} FAIL"
+                msgne -blanco "Puerto Elegido: "
+                msg -rojo "${TTOTAL2[$i]} FAIL"
             }
         done
         [[ -z $PORT2 ]] && {
-            echo -e "\033[1;31m Ningun Puerto Valido Fue Elegido\033[0m"
-            return 1
+            msgCentrado -rojo "Ningun Puerto Valido Fue Elegido"
+            activar_dropbear
         }
 
         msg -bar
-        echo -e "\033[1;97m Revisando Actualizaciones"
-        fun_bar "apt update; apt upgrade -y > /dev/null 2>&1"
-        echo -e "\033[1;97m Instalando Dropbear"
-        fun_bar "apt-get install dropbear -y > /dev/null 2>&1"
-        apt-get install dropbear -y >/dev/null 2>&1
+
+        msgInstall -blanco "Revisando Actualizaciones"
+        fun_bar "apt update; apt upgrade -y"
+
+        msgInstall -blanco "Instalando Dropbear"
+        fun_bar "apt-get install dropbear -y"
+
         touch /etc/dropbear/banner
         msg -bar
         cat <<EOF >/etc/default/dropbear
@@ -51,33 +54,23 @@ EOF
         sed -i "s/=1/=0/g" /etc/default/dropbear
         service dropbear restart
         sed -i "s/=0/=1/g" /etc/default/dropbear
-        sleep 3s
-        echo -e "\033[1;92m        >> DROPBEAR INSTALADO CON EXITO <<"
-        msg -bar
 
+        msgSuccess
     }
 
     desactivar_dropbear() {
-        clear && clear
-        msg -bar
-        echo -e "\033[1;91m              DESINSTALANDO DROPBEAR"
-        msg -bar
+        showCabezera "DESINSTALANDO DROPBEAR"
+
         service dropbear stop >/dev/null 2>&1
         fun_bar "apt-get remove dropbear -y"
         killall dropbear >/dev/null 2>&1
         rm -rf /etc/dropbear/* >/dev/null 2>&1
-        msg -bar
-        echo -e "\033[1;32m             DROPBEAR DESINSTALADO EXITO"
-        msg -bar
         [[ -e /etc/default/dropbear ]] && rm /etc/default/dropbear
+
+        msgSuccess
     }
 
-    clear && clear
-    msg -bar
-    msg -tit
-    msg -bar
-    msgCentrado -amarillo "INSTALADOR DROPBEAR | SCRIPT LXServer"
-    msg -bar
+    showCabezera "INSTALADOR DROPBEAR | SCRIPT LXServer"
 
     local num=1
     # INSTALAR DROPBEAR
