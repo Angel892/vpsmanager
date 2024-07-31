c1e6e0b629b7a84cf7287d75c6c42e2dd8da6fec
LXServer <35785095+Angel892@users.noreply.github.com>
2024-07-31 15:31:12 -0600

    feat: ssh

--- adm.sh
+++ adm.sh
@@ -80,7 +80,7 @@ mainMenu() {
     "autoIniciar") autoiniciarScript ;;
     "puertos") mostrarPuertosActivos ;;
     "volver")
-        echo -e "${INFO}Saliendo...${NC}" && exit
+        exit
         ;;
     *)
         echo -e "${SALIR}Opción inválida, por favor intente de nuevo.${NC}"
