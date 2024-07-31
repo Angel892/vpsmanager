8a5e8727030604a31b23205dc8a7b10dcbf68749
LXServer <35785095+Angel892@users.noreply.github.com>
2024-07-31 10:57:02 -0600

    feat: mysql

--- functions/protocols/manage.sh
+++ functions/protocols/manage.sh
@@ -186,7 +186,7 @@ menuProtocols() {
     "ppublico") proto_ppublico ;;
     "pprivado") proto_pprivado ;;
     "python") menuPython ;;
-    "volver") return ;;
+    "volver") return 1 ;;
     *)
         echo -e "${SALIR}Opción inválida, por favor intente de nuevo.${NC}"
         menuProtocols
