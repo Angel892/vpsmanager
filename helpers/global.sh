400d22188ca3dd92d8241c6142a01946dd03208b
LXServer <35785095+Angel892@users.noreply.github.com>
2024-08-01 10:29:40 -0600

    feat: ssl

--- helpers/global.sh
+++ helpers/global.sh
@@ -123,7 +123,8 @@ msgCentrado() {
     case $1 in
     -ne) cor="${NEGRITA}${ROJO}${NEGRITO}" && echo -ne "${cor}${2}${SINCOLOR}" ;;
     -amarillo) cor="${NEGRITA}${AMARILLO}${NEGRITO}" && echo -e "${cor}${2}${SINCOLOR}" ;;
-    -blanco) cor="${NEGRITA}${BLANCO}${NEGRITO}[!] ${ROJO}" && echo -e "${cor}${2}${SINCOLOR}" ;;
+    -blanco) cor="${NEGRITA}${BLANCO}${NEGRITO}" && echo -e "${cor}${2}${SINCOLOR}" ;;
+    -advertencia) cor="${NEGRITA}${BLANCO}${NEGRITO}[!] ${ROJO}" && echo -e "${cor}${2}${SINCOLOR}" ;;
     -rojo) cor="${NEGRITA}${ROJO}${NEGRITO}" && echo -e "${cor}${2}${SINCOLOR}" ;;
     -azul) cor="${NEGRITA}${AZUL}${NEGRITO}" && echo -e "${cor}${2}${SINCOLOR}" ;;
     -verde) cor="${NEGRITA}${VERDE}${NEGRITO}" && echo -e "${cor}${2}${SINCOLOR}" ;;
