bde92d16ed3db57588f9c884db936f646a6033c3
LXServer <35785095+Angel892@users.noreply.github.com>
2024-08-02 12:51:47 -0600

    feat: v2ray

--- functions/v2ray/uninstall.sh
+++ functions/v2ray/uninstall.sh
@@ -1,5 +1,5 @@
 unistallv2() {
-    source <(curl -sL https://multi.netlify.app/v2ray.sh) --remove >/dev/null 2>&1
+    source <(curl -sL https://github.com/Angel892/vpsmanager/raw/master/install/v2ray/v2ray.sh) --remove >/dev/null 2>&1
     rm -rf $mainPath/RegV2ray >/dev/null 2>&1
     rm -rf $mainPath/v2ray/* >/dev/null 2>&1
     msgCentrado --verde "V2RAY DESINSTALADO CON EXITO"
