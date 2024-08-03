b4287854f0de5091219c6f1c4ae6ccbd998d5377
Angel Amaro <alex.jaj94@gmail.com>
2024-08-03 15:13:00 -0600

    feat: avance

--- functions/v2ray/showUsers.sh
+++ functions/v2ray/showUsers.sh
@@ -1,43 +1,41 @@
 mosusr_kk() {
     showCabezera "USUARIOS REGISTRADOS | UUID V2RAY"
-    # usersss=$(cat $mainPath/RegV2ray|cut -d '|' -f1)
-    # cat $mainPath/RegV2ray|cut -d'|' -f3
-    VPSsec=$(date +%s)
+    
     local HOST="$mainPath/RegV2ray"
-    local HOST2="$mainPath/RegV2ray"
-    local RETURN="$(cat $HOST | cut -d'|' -f2)"
-    local IDEUUID="$(cat $HOST | cut -d'|' -f1)"
-    if [[ -z $RETURN ]]; then
+    if [[ ! -f $HOST || ! -s $HOST ]]; then
         echo -e "----- NINGUN USER REGISTRADO -----"
-        msg -ne "Enter Para Continuar" && read enter
+        msg -ne "Enter Para Continuar" && read -r
         return
-    else
-        i=1
-        echo -e "\e[97m                 UUID                | EMAIL | USER | DIAS\e[93m"
-        msg -bar
-        while read hostreturn; do
-            DateExp="$(cat $mainPath/RegV2ray | grep -w "$hostreturn" | cut -d'|' -f4)"
-            if [[ ! -z $DateExp ]]; then
-                DataSec=$(date +%s --date="$DateExp")
-                [[ "$VPSsec" -gt "$DataSec" ]] && EXPTIME="\e[91m[EXPIRADO]\e[97m" || EXPTIME="\e[92m[$(($(($DataSec - $VPSsec)) / 86400))]"
-            else
-                EXPTIME="\e[91m[ S/R ]"
-            fi
-            usris="$(cat $mainPath/RegV2ray | grep -w "$hostreturn" | cut -d'|' -f3)"
-            usremail="$(cat $mainPath/RegV2ray | grep -w "$hostreturn" | cut -d'|' -f2)"
-            local contador_secuencial+="\e[93m$hostreturn \e[97m|\e[93m $usremail\e[97m|\e[93m$usris\e[97m|\e[93m $EXPTIME \n"
-            if [[ $i -gt 30 ]]; then
-                echo -e "$contador_secuencial"
-                unset contador_secuencial
-                unset i
-            fi
-            let i++
-        done <<<"$IDEUUID"
-
-        [[ ! -z $contador_secuencial ]] && {
-            linesss=$(cat $mainPath/RegV2ray | wc -l)
-            echo -e "$contador_secuencial \n \e[1;97mNumero de Registrados: $linesss"
-        }
     fi
+
+    local VPSsec=$(date +%s)
+    local contador_secuencial=""
+    local i=1
+
+    echo -e "\e[97m                 UUID                | EMAIL | USER | DIAS\e[93m"
+    msg -bar
+
+    while IFS='|' read -r uuid email user dateExp; do
+        if [[ -n $dateExp ]]; then
+            local DataSec=$(date +%s --date="$dateExp")
+            local EXPTIME=$([[ "$VPSsec" -gt "$DataSec" ]] && echo -e "\e[91m[EXPIRADO]\e[97m" || echo -e "\e[92m[$(( (DataSec - VPSsec) / 86400 ))]\e[97m")
+        else
+            local EXPTIME="\e[91m[ S/R ]\e[97m"
+        fi
+
+        contador_secuencial+=$(printf "\e[93m%s \e[97m|\e[93m %s\e[97m|\e[93m%s\e[97m|\e[93m %s \n" "$uuid" "$email" "$user" "$EXPTIME")
+
+        if (( i % 30 == 0 )); then
+            echo -e "$contador_secuencial"
+            contador_secuencial=""
+        fi
+        ((i++))
+    done < "$HOST"
+
+    if [[ -n $contador_secuencial ]]; then
+        local linesss=$(wc -l < "$HOST")
+        echo -e "$contador_secuencial \n \e[1;97mNumero de Registrados: $linesss"
+    fi
+
     msgSuccess
 }
