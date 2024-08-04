45f4b45cb3186da2869eaf6ff0a1259b1e4804b0
Angel Amaro <alex.jaj94@gmail.com>
2024-08-04 15:14:53 -0600

    feat: avance

--- auto/limitadorconexionesv2ray.sh
+++ auto/limitadorconexionesv2ray.sh
@@ -7,13 +7,8 @@ limitadorv2ray() {
 
     while IFS='|' read -r uuid email user limite dateExp; do
 
-        # Inicializa un array para almacenar las IPs únicas
-        unique_ips=()
-
         # Usa awk para procesar el archivo y extraer las IPs únicas, luego almacénalas en el array
-        ips_output=$(grep "$email" /var/log/v2ray/access.log | awk '{match($0, /([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+):/, ip); if (ip[1] != "") print ip[1]}' | sort | uniq)
-
-        mapfile -t unique_ips <<< "$ips_output"
+        readarray -t unique_ips < <(grep "$email" /var/log/v2ray/access.log | awk '{match($0, /([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+):/, ip); if (ip[1] != "") print ip[1]}' | sort | uniq)
 
         # Cuenta las IPs únicas
         unique_ip_count=${#unique_ips[@]}
