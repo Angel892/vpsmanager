5edc3297bf24d20402d5931b4b9fae5bbb6086c8
LXServer <35785095+Angel892@users.noreply.github.com>
2024-08-13 16:07:55 -0600

    feat: site manager

--- functions/protocols/manage.sh
+++ functions/protocols/manage.sh
@@ -38,7 +38,7 @@ menuProtocols() {
     let num++
 
     # NGINX
-    opcionMenu -blanco $num "Nginx" true 0 "nginx" "f"
+    opcionMenu -blanco $num "Nginx" true 0 "nginx" "v"
     option[$num]="nginx"
     let num++
 
